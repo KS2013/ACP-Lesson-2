3fdfae4704aaa2eb993894110e410a04503a0e91
KS2013 <ksarker2013@gmail.com>
2024-11-07 21:07:50 +0000

    Add files via upload

---
+++ HW.py
@@ -0,0 +1,7 @@
+achievment = input("Have you achieved anything significant in the past few days? (y/n) ")
+congrats = "Congractulations"
+
+if achievment == "y":
+  print(congrats.upper())
+else:
+  print("Well try hard and maybe you could achieve something significamt")
